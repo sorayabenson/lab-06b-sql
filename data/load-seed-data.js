b69a12666f02e6c70d7ae95ecd2302a50f9e892b
Soraya Benson <soraya.benson@gmail.com>
2021-02-23 15:48:33 -0800

    endpoints written

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,8 +1,10 @@
 const client = require('../lib/client');
 // import our seed data:
 const ghosts = require('./ghosts.js');
+const categoriesData = require('./categories.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
+const categories = require('./categories.js');
 
 run();
 
@@ -20,20 +22,31 @@ async function run() {
                   `,
         [user.email, user.hash]);
       })
+    );  
+    
+    await Promise.all(
+      categoriesData.map(category => {
+        return client.query(`
+                      INSERT INTO categories (id, category_name)
+                      VALUES ($1, $2)
+                      RETURNING *;
+                  `,
+        [category.id, category.category_name]);
+      })
     );
-      
+    
     const user = users[0].rows[0];
 
     await Promise.all(
       ghosts.map(ghost => {
         return client.query(`
-                    INSERT INTO ghosts (name, img, description, category, price, price_currency, trustworthy, owner_id)
+                    INSERT INTO ghosts (name, img, description, category_id, price, price_currency, trustworthy, owner_id)
                     VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
                 `,
         [ghost.name, 
           ghost.img,
           ghost.description,
-          ghost.category,
+          ghost.category_id,
           ghost.price,
           ghost.price_currency,
           ghost.trustworthy,
